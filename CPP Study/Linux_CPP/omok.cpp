c0a843ffff1a2848ffb0b46894a0b95a7c8dca32
최현준 <chj878194@naver.com>
2017-07-10 21:04:39 +0900

    Compile Error Correction.
    - hpp + cpp relation correct.
    - using g++, not gcc to compile .cpp files.

--- CPP Study/Linux_CPP/omok.cpp
+++ CPP Study/Linux_CPP/omok.cpp
@@ -1,3 +1,84 @@
-#include<omok>
+#include"omok.hpp"
+#include<iostream>
+#include <stdio.h>
+#include <termio.h>
 
+using namespace std;
 
+int getch(){
+    int ch;
+    struct termios buf, save;
+    tcgetattr(0,&save);
+
+    buf = save;
+    buf.c_lflag &= ~(ICANON|ECHO);
+    buf.c_cc[VMIN] = 1;
+    buf.c_cc[VTIME] = 0;
+
+    tcsetattr(0, TCSAFLUSH, &buf);
+    
+    ch = getchar();
+    tcsetattr(0, TCSAFLUSH, &save);
+    return ch;
+}
+
+Location Player::MakeDecision(GameBoard Board)
+{
+    return Location(0, 0);
+}
+
+Location Player::MoveCursor(Location where)
+{
+    return Location(0, 0);
+}
+
+void GameManager::GameStart()   
+{
+
+}
+
+Player GameManager::WhosTurn()
+{
+    return Player();
+}
+
+Player GameManager::WhosWin()
+{
+
+}
+
+bool GameManager::IsFinished()
+{
+    return true;
+}
+
+Location GameManager::GetPlayerDicision(Player p)
+{
+    return Location(0, 0);
+}
+
+void GameManager::PrintBoard()
+{
+
+}
+
+bool GameBoard::MoveCursor(Location Where)  
+{
+    return true;
+}
+
+int GameBoard::WhosWin()  
+{
+    return BLACK;
+}
+
+bool GameBoard::SetStone(Location where)  
+{
+    return true;
+}
+
+int main() {
+    cout << "Hello World!";
+
+    return 0;
+}
--- CPP Study/Linux_CPP/omok.hpp
+++ CPP Study/Linux_CPP/omok.hpp
@@ -1,44 +1,121 @@
 #include<vector>
+#include<iostream>
 
 #define BLACK 1
 #define WHITE 2
 #define EMPTY 3
 #define BLOCK 0
 
+using namespace std;
+
+int getch();
+
+// ●○★☆□■△▲▽▼→←↑↓◁◀▷▶┼┌┐┘└┬┤├┴ 
+// αβγδεθω
+
+class Location
+{
+private:
+	int x, y;
+public:
+	Location() 
+	{
+		this -> x = 0;
+		this -> y = 0;
+	}
+
+	Location(int x, int y)
+	{
+		this -> x = x;
+		this -> y = y;
+	}
+
+	int GetX()	{
+		return x;
+	}
+
+	int GetY()	{
+		return y;
+	}
+
+	void SetXY(int x, int y)	{
+		this->x = x;
+		this->y = y;
+	}
+
+	void SetX(int x)	{
+		this->x = x;
+	}
+
+	void SetY(int y)	{
+		this->y = y;
+	}
+};
+
 class Square
 {
 public:
 	void PrintSquare();
 	int GetSquareType();
-}
+};
 
 class BlackStone:Square
 {
+public:
+	void PrintSquare()	{
+		cout << "●";
+	}
 	int GetSquareType()	{
 		return BLACK;
 	}
-}
+};
 
 class WhiteStone:Square
 {
+public:
+	void PrintSquare()	{
+		cout << "○";
+	}
 	int GetSquareType()	{
 		return WHITE;
 	}
-}
+};
 
 class EmptyStone:Square
 {
+public:
+	void PrintSquare()	{
+		cout << "┼";
+	}
 	int GetSquareType()	{
 		return EMPTY;
 	}
-}
+};
 
 class BlockStone:Square
 {
+public:
+	void PrintSquare()	{
+		cout << "■";
+	}
 	int GetSquareType()	{
 		return BLOCK;
 	}
-}
+};
+
+class GameBoard
+{
+public:
+	bool MoveCursor(Location Where);
+	int WhosWin();
+	bool SetStone(Location where);
+private:
+	Location cursor;
+	vector<Location> actionHistory;
+	vector<int> stoneHistory;
+	int width, height;
+	Square board[19][19];
+};
 
 class Player
 {
@@ -46,33 +123,21 @@ public:
 	Location MakeDecision(GameBoard Board);
 	Location MoveCursor(Location where);
 	int GetPlayerType();
-}
+};
 
 class BlackPlayer:Player
 {
 	int GetPlayerType()	{
 		return BLACK;
 	}
-}
+};
 
 class WhitePlayer:Player
 {
 	int GetPlayerType()	{
 		return WHITE;
 	}
-}
-
-class Location
-{
-private:
-	int x, y;
-public:
-	int GetX();
-	int GetY();
-	void SetXY(int x, int y);
-	void SetX(int x);
-	void SetY(int y);
-}
+};
 
 class GameManager
 {
@@ -80,23 +145,10 @@ public:
 	void GameStart();
 	Player WhosTurn();
 	Player WhosWin();
-	boolean IsFinished();
+	bool IsFinished();
 	Location GetPlayerDicision(Player p);
 	void PrintBoard();
 private:
 	Player m_a, m_b;
 	GameBoard board;
-}
-
-class GameBoard
-{
-public:
-	boolean MoveCursor(Location Where);
-	Player WhosWin();
-	boolean SetStone(Location where);
-private:
-	Location cursor;
-	vector<Location> actionHistory;
-	int width, height;
-	Square board[19][19];
-}
+};
