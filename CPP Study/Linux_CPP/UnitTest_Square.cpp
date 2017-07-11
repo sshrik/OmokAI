db6588c505796bf5783c3372b48ec23d0e013b86
최현준 <chj878194@naver.com>
2017-07-11 18:53:04 +0900

    Square Unit Test Finished.
    Square Unit Test Finished.

---
+++ CPP Study/Linux_CPP/UnitTest_Square.cpp
@@ -0,0 +1,50 @@
+#include"omok.cpp"
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+// #include<time.h>
+
+using namespace std;
+
+int main()  {
+    Square* board[MAX_PAN][MAX_PAN];
+    int type = 0;
+    srand((unsigned int)time(NULL));
+
+    for(int i = 0; i < MAX_PAN; i++)    {
+        for(int j = 0; j < MAX_PAN; j++)    {
+            type = rand() % 4;
+            cout << "Board has been declared... ";
+            if(type == BLOCK)   {
+                cout << "BLOCK in (" << i << ", " << j << ")" << endl;
+                board[i][j] = new BlockStone;
+            }
+            else if(type == BLACK)   {
+                cout << "BLACK in (" << i << ", " << j << ")" << endl;
+                board[i][j] = new BlackStone;
+            }
+            else if(type == WHITE)   {
+                cout << "WHITE in (" << i << ", " << j << ")" << endl;
+                board[i][j] = new WhiteStone;
+            }
+            else {
+                cout << "EMPTY in (" << i << ", " << j << ")" << endl;
+                board[i][j] = new EmptyStone;
+            }
+        }
+    }
+
+    for(int i = 0; i < MAX_PAN; i++)    {
+        for(int j = 0; j < MAX_PAN; j++)    {
+            board[i][j] -> PrintSquare();
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < MAX_PAN; i++)    {
+        for(int j = 0; j < MAX_PAN; j++)    {
+            delete board[i][j];
+        }
+    }
+    
+}
